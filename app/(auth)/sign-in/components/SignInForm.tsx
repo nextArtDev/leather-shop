33b40e0dbb07ca67e82e252ce1c89742147f963d
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2025-07-29 00:09:03 +0330

    sign in page

---
+++ app/(auth)/sign-in/components/SignInForm.tsx
@@ -0,0 +1,72 @@
+'use client'
+import { useState } from 'react'
+import { toast } from 'sonner'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+import { cn } from '@/lib/utils'
+import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { PhoneInput } from '@/components/ui/phone-input'
+
+const formSchema = z.object({
+  name_5339199072: z.string(),
+})
+
+export default function SignInForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      console.log(values)
+      toast(
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(values, null, 2)}</code>
+        </pre>
+      )
+    } catch (error) {
+      console.error('Form submission error', error)
+      toast.error('Failed to submit the form. Please try again.')
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 max-w-3xl mx-auto py-10"
+      >
+        <FormField
+          control={form.control}
+          name="name_5339199072"
+          render={({ field }) => (
+            <FormItem className="flex flex-col items-start">
+              <FormLabel>Phone number</FormLabel>
+              <FormControl className="w-full">
+                <PhoneInput
+                  placeholder="9352310831"
+                  {...field}
+                  defaultCountry="IR"
+                />
+              </FormControl>
+              <FormDescription>Enter your phone number.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  )
+}
---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SignInForm from './components/SignInForm'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <div>
+      <SignInForm />
+    </div>
+  )
+}
+
+export default page
